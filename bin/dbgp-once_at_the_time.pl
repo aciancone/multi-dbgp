1b9be94a5c1ca4994c799c5c3e95a6a2f0f0f81e
Andrea Ciancone <aciancone@gmail.com>
2014-12-30 16:19:37 +0100

    By default multi-dbgp interacts with debuggers and ide via socket

--- bin/dbgp-once_at_the_time.pl
+++ bin/dbgp-once_at_the_time.pl
@@ -14,9 +14,9 @@ my $cv = AE::cv;
 
 my $dispatcher = new MultiDbgp::Dispatcher::LazySessionSelection( {
 	debugger_host	=> 'unix/',
-	debugger_port	=> '/var/run/dbgp/uwsgi',
-	client_host	=> "localhost",
-	client_port	=> 9000,
+	debugger_port	=> '/var/run/dbgp/uwsgi.sock',
+	client_host	=> "unix/",
+	client_port	=> "/var/run/dbgp/ide.sock",
 } );
 
 
